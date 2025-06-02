9af3d5979339cd944413fc964f841215c0deee6d
Reed Lathrop <reedlathrop@gmail.com>
2025-06-02 10:38:12 -0500

    removed local

--- app.py
+++ app.py
@@ -10,10 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://fitess-tracker.vercel.app",
-    ],
+    allow_origins=["https://fitess-tracker.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
